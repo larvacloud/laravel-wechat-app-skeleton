267730ba35e937af598fd2dd9e9c4926fd618ccf
XuTongle <xutongle@gmail.com>
2020-06-30 00:12:17 +0800

    微信接口后续处理

--- api/request.js
+++ api/request.js
@@ -8,23 +8,25 @@ export default function request(params) {
             url: config.baseUrl + params.url || '',
             data: params.data || {},
             header: {
+				'Accept': 'application/json',
                 'Content-Type': 'application/json',
                 Authorization: `Bearer ${wx.getStorageSync('access_token')}`
             },
             success(res) {
-                if (res.statusCode !== 200) {
-                    wx.showToast({
+				if(res.statusCode == 200) {
+                    resolve(res.data || {});
+                } else if(res.statusCode == 404) {
+                    swan.showToast({
                         icon: 'none',
-                        title: '系统错误'
+                        title: '服务接口不存在！'
                     });
-                    reject('系统错误');
-                    return;
-                }
-                if (res.data.code === 0) {
-                    resolve(res.data.data || {})
                 } else {
-                    reject(res.data.message)
+                    swan.showToast({
+                        icon: 'none',
+                        title: res.data.message || '服务器繁忙！'
+                    });
                 }
+                reject(res.data.message)
             },
             fail(err) {
                 reject(err)
